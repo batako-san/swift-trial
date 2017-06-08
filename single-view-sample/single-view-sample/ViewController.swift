6623d4565d51b351ec27c580d5beaefbda3a1d39
Hiroto Kawabata <hkawabat@yahoo-corp.jp>
2017-06-08 23:19:26 +0900

    display image

--- single-view-sample/single-view-sample/ViewController.swift
+++ single-view-sample/single-view-sample/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        // 画像ファイルを読み込み
+        let image = UIImage(named: "swift_logo.png")
+        // ImageView に画像を設定
+        imageView.image = image
     }
 
     override func didReceiveMemoryWarning() {
